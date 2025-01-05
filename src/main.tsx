5d72ac390f74f23786f1d0a8886c84a4d52ee079
João Graça <jgraca95@gmail.com>
2025-01-05 13:42:45 +0000

    implement vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
     <MantineProvider defaultColorScheme="dark">
       <Notifications />
       <App />
+      <SpeedInsights />
     </MantineProvider>
   </StrictMode>,
 )
