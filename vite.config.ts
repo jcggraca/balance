f68ea75c852c090c07b1c6af0012a7aaa08a190f
João Graça <mail@joaograca.dev>
2026-04-02 22:11:27 +0100

    Update to vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,76 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import type { ManifestOptions } from 'vite-plugin-pwa'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import manifest from './manifest.json'
 
-// https://vitejs.dev/config/
+const manifest: Partial<ManifestOptions> = {
+  name: 'Balance',
+  short_name: 'Balance',
+  description: 'Balance your money and time',
+  theme_color: '#242424',
+  background_color: '#242424',
+  display: 'standalone',
+  scope: '/',
+  start_url: '/',
+  icons: [
+    {
+      src: 'favicon.svg',
+      sizes: 'any',
+      type: 'image/svg+xml',
+      purpose: 'any monochrome',
+    },
+    {
+      src: 'favicon-96x96.png',
+      sizes: '96x96',
+      type: 'image/png',
+    },
+    {
+      src: 'favicon.ico',
+      sizes: '96x96',
+      type: 'image/x-icon',
+    },
+    {
+      src: 'apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+    },
+    {
+      src: '/web-app-manifest-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+    {
+      src: '/web-app-manifest-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+  ],
+  screenshots: [
+    {
+      src: 'screen-capture.png',
+      sizes: '640x320',
+      type: 'image/png',
+      form_factor: 'wide',
+      label: 'Screenshot',
+    },
+    {
+      src: 'screen-capture-expenses.png',
+      sizes: '640x320',
+      type: 'image/png',
+      form_factor: 'wide',
+      label: 'Screenshot Expenses',
+    },
+  ],
+}
+
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths(), VitePWA({
+  plugins: [tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true,
+  }), react(), VitePWA({
     registerType: 'autoUpdate',
     manifest,
     includeAssets: ['**/*'],
