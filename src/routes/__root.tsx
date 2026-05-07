0be72b47b8116ef2d57e77889198eae825a59861
João Graça <mail@joaograca.dev>
2026-05-07 01:02:32 +0100

    Update dependencies

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,13 @@
 import { createRootRoute } from '@tanstack/react-router'
-import { lazy, Suspense } from 'react'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import Layout from '../layout/Layout'
 import ErrorNotFound from '../pages/ErrorNotFound'
-
-const TanStackRouterDevtools = import.meta.env.VITE_ENV === 'production'
-  ? () => null
-  : lazy(() =>
-      import('@tanstack/router-devtools').then(res => ({
-        default: res.TanStackRouterDevtools,
-      })),
-    )
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Layout />
-      <Suspense>
-        <TanStackRouterDevtools />
-      </Suspense>
+      <TanStackRouterDevtools />
     </>
   ),
   notFoundComponent: () => <ErrorNotFound />,
